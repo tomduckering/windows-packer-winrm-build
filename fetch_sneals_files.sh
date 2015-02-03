8d6f40078ca6148d62b7dde4dbb6db0923367e6c
Tom Duckering <tduckeri@thoughtworks.com>
2015-02-03 17:57:06 +0000

    Removing files not pertinent to building branched packer.

--- fetch_sneals_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -OL https://gist.githubusercontent.com/sneal/a1ef4e9d5c2f86f8580b/raw/d1285e9e510a53a784916fd89352ff761d1f94ea/Autounattend.xml
-curl -OL https://gist.githubusercontent.com/sneal/39d47401e9eaefcf8727/raw/63fca256afda07591aa3cb5bba370e77f3b78723/configure-winrm.ps1
-curl -L https://gist.githubusercontent.com/sneal/de23df76382708b132ce/raw/553835e9d6c660ad820a7c1f340e17b1b5dce471/instal-chef.ps1 -o install-chef.ps1
-curl -OL https://gist.githubusercontent.com/sneal/73a0a088c55fbbe07ec6/raw/c7ae5356fd55a3de4fc937b55f79f839d511cefe/install-7zip.ps1
-curl -OL https://gist.githubusercontent.com/sneal/eeb52e859186ba44163d/raw/f2d90d497c8e77883fa2d54565d8ee4cbfde252c/install-vmware-tools.ps1
